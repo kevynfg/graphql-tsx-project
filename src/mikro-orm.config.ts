e7bddc5bc2ffbd94b2fe360ba54fe431622554e9
Kevyn Gonçalves <kevyn.goncalves@fcamara.com.br>
2021-10-28 12:27:56 -0300

    add: migrations, resolver, types, dependencies, first crud operations, graphql and server start

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+
+export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'), // path to the folder with migrations
+        pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+    },
+    entities: [Post],
+    dbName: 'jellyfish',
+    user: "postgres",
+    password: "postgres",
+    type: 'postgresql',
+    debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
