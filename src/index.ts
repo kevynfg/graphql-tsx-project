16baa63590e6ca9424c32d016d2e7a9349c72931
Kevyn Gonçalves <kevynredlotus@gmail.com>
2021-11-04 15:05:04 -0300

    add: Redis, cookie, new dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,64 @@
-import 'reflect-metadata';
-import { MikroORM } from '@mikro-orm/core';
-import express from 'express';
-import { __prod__ } from './constants';
-import mikroConfig from './mikro-orm.config';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import { PostResolver } from './resolvers/post';
-import { UserResolver } from './resolvers/user';
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { __prod__ } from "./constants";
+import mikroConfig from "./mikro-orm.config";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
 
 const main = async () => {
-    const orm = await MikroORM.init(mikroConfig);
-    await orm.getMigrator().up();
-    
-    const app = express();
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [PostResolver, UserResolver],
-            validate: false
-        }),
-        context: ({em: orm.em})
-    });
-    await apolloServer.start();
-    apolloServer.applyMiddleware({app});
-    
-    app.listen(4000, () => {
-        console.log('server started on localhost:4000')
-    });
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
+        httpOnly: true, //disable front-end access for cookies
+        sameSite: "lax", //csrf
+        secure: __prod__, //cookie only works in https
+      },
+      secret: "idontknowhow",
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({
+      em: orm.em,
+      req,
+      res,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main();
-
-console.log('Hello orldaaaaaaaaaa')
