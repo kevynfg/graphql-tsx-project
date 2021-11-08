14ab9a71f26c438492fedd2b3c4b0327324b1046
Kevyn Gonçalves <kevynredlotus@gmail.com>
2021-11-08 09:08:00 -0300

    fix: user resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -99,7 +99,7 @@ export class UserResolver {
         try {
             await em.persistAndFlush(user);
         } catch (err) {
-            if (err.code === "23505") {
+            if (err.detail.includes("already exists")) {
                 return {
                     errors: [
                         {
