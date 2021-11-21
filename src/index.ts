583e502f401e0fe16d7ee2007e1fd305ef98f277
Kevyn Gonçalves <kevynredlotus@gmail.com>
2021-11-21 18:41:44 -0300

    add: posts mock, post resolver,

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,10 @@ import cors from "cors";
 import { createConnection } from "typeorm";
 import { Post } from "./entities/Post";
 import { User } from "./entities/User";
+import path from "path";
 
 const main = async () => {
-    await createConnection({
+    const conn = await createConnection({
         type: "postgres",
         database: "jellyfish2",
         username: "postgres",
@@ -22,8 +23,9 @@ const main = async () => {
         logging: true,
         synchronize: true,
         entities: [Post, User],
+        migrations: [path.join(__dirname, "./migrations/*")],
     });
-
+    // await conn.runMigrations();
     const app = express();
 
     const RedisStore = connectRedis(session);
